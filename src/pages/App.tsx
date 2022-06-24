c8714f4da1b7197ea4a2341ebce0a09321719f9d
Thud <93019274+cryptothud@users.noreply.github.com>
2022-06-24 10:31:28 -0700

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -181,8 +181,11 @@ export default function App() {
       <AppWrapper>
         <img src="https://i.ibb.co/2ZN5KSQ/IMG-5784-Moment.png" alt="" id="newbg" />
         <div className="backgroundVid">
-          <video loop autoPlay>
-            <source src="/images/sojo/bgvid.mp4" type="video/mp4" />
+          <video loop autoPlay muted>
+            <source
+              src="/images/sojo/bgvid.mp4"
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
         </div>
